fb48d6fc912f66d7134fb3b06426d4a0a5b1567c
chillpill915 <chillpill915@gmail.com>
2025-02-16 19:35:33 -0600

    hopefully my code is pretty

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,5 +1,5 @@
 //custom javascript will go here
-document.querySelectorAll('.preview-image').forEach(img => {
-    img.style.width = '200px';  // Set your desired width
-    img.style.height = 'auto'; // Maintain aspect ratio
-  });
+document.querySelectorAll(".preview-image").forEach((img) => {
+  img.style.width = "200px"; // Set your desired width
+  img.style.height = "auto"; // Maintain aspect ratio
+});
